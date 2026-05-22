dcd14b8c371266c16c5e2ad7ba4c6b6192757bd3
Pavel <hidukoff-pavel@yandex.ru>
2026-05-22 19:41:47 +0200

    Initial Commit

---
+++ KhaidukovPA_Kursash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KhaidukovPA_Kursash
+//
+//  Created by user271126 on 5/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KhaidukovPA_Kursash/KhaidukovPA_KursashApp.swift
@@ -0,0 +1,17 @@
+//
+//  KhaidukovPA_KursashApp.swift
+//  KhaidukovPA_Kursash
+//
+//  Created by user271126 on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct KhaidukovPA_KursashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
